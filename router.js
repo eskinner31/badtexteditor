b2de3e43a0d7ce92643acc1ec7f87c79f61e61bb
Eugene Skinner <eskinfrn@gmail.com>
2015-07-07 15:09:20 -0600

    Routes Are Functional

--- router.js
+++ router.js
@@ -1,18 +1,10 @@
 var routes = require('routes')()
 var fs = require('fs')
-var db = require('monk')('localhost/movies')
-var movies = db.get('movies')
+var db = require('monk')('localhost/notes')
+var notes = db.get('notes')
 var qs = require('qs')
 var view = require('./view')
 var mime = require('mime')
-
-routes.addRoute('/start', (req, res, url) => {
-  res.setHeader('Content-Type', 'text/html')
-    if (req.method === 'GET') {
-        var template = view.render('home', {} )
-        res.end(template)
-  }
-})
 
 routes.addRoute('/public/*', function (req, res, url) {
   res.setHeader('Content-Type', mime.lookup(req.url))
@@ -25,117 +17,111 @@ routes.addRoute('/public/*', function (req, res, url) {
     res.end(file)
   })
 })
-
-routes.addRoute('/movies', (req, res, url) => {
-  console.log('in /movies')
+routes.addRoute('/notes/index', (req, res, url) => { //home index
+  console.log("working with mustache")
+  console.log('in /notes/index')
   res.setHeader('Content-Type', 'text/html')
     if (req.method === 'GET') {
-        movies.find({}, function (err, docs) {
+        notes.find({}, function (err, docs) {
           if (err) {
             res.end('shits broke yo')
             return
           }
-          var template = view.render('index', {movies: docs} )
+          var template = view.render('index', {notes: docs} )
+          console.log('working with mustache')
           res.end(template)
+          console.log('done')
           return
         })
       }
     res.setHeader('Content-Type', 'text/html')
     if (req.method === 'GET') {
-      movies.find({}, function (err, docs) {
+      notes.find({}, function (err, docs) {
         if (err) {
           res.end('shits broke yo')
           return
         }
-        var template = view.render('index', { movies: docs} )
+        var template = view.render('index', docs )
         res.end(template)
       })
     }
-  if (req.method == 'POST') {
-    var data = ''
-    req.on('data', function (chunk) {
-      data += chunk
-    })
-    req.on('end', function () {
-      var movie = qs.parse(data)
-      console.log(movie)
-      movies.insert(movie, function (err, doc) {
-        if (err) {
-          console.log(err)
-          res.end('It may have broke vOv')
-          return
-        }
-        res.writeHead(302, {'Location': '/movies'})
-        res.end()
-      })
-    })
-  }
 })
-
-routes.addRoute('/movies/new', (req, res, url) => {
-  console.log('in movies/new')
+routes.addRoute('/notes/new', (req, res, url) => {
+  console.log('in new')
   res.setHeader('Content-Type', 'text/html')
   var template = view.render('new', {} )
   res.end(template)
 })
-routes.addRoute('/movies/:id/update', (req, res, url) => {
-  console.log('loaded update page')
-  if (req.method === 'GET')
-  console.log(url.params.id)
-  var movie = movies.findOne({_id: url.params.id}, function (err, doc) {
-    if (err) {
-      res.end('shits broke yo')
-      return
-    }
-    res.setHeader('Content-Type', 'text/html')
-    var template = view.render('update', doc )
-    res.end(template)
-  })
+
+routes.addRoute('/notes/:id', (req, res, url) => {
+  res.setHeader('Content-Type', 'text/html')
+  if(req.method === "GET"){
+    notes.findOne({_id: url.params.id}, function(err,doc){
+      if(err){
+        res.end("Shits Broke Yo")
+        return
+      }
+      var template = view.render('show', doc)
+      res.end(template);
+    })
+  }
 })
-routes.addRoute('/movies/:id/delete', (req, res, url) => {
+
+routes.addRoute('/notes/:id/delete', (req, res, url) => {
   console.log('invoke delete')
 
   if (req.method === 'POST') {
-    movies.remove({_id: url.params.id}, function (err, doc) {
+    notes.remove({_id: url.params.id}, function (err, doc) {
       if (err) {
         res.end('well, fuck')
         return
       }
-      res.writeHead(302, {'Location': '/movies'})
+        res.writeHead(302, {'Location': '/notes/index'})
       res.end()
     })
   }
 })
-routes.addRoute('/movies/:id', (req, res, url) => {
-  console.log('in movies/id/edit')
+routes.addRoute('/notes/:id/update', (req, res, url) => {
+  console.log('in notes/id/edit')
   if (req.method === 'POST'){
       var data = ''
       req.on('data', function (chunk){
           data += chunk
       })
       req.on('end', function () {
-        var movie = qs.parse(data)
-        movies.update({_id: url.params.id}, movie, function (err, doc){
+        var note = qs.parse(data)
+        notes.update({_id: url.params.id}, note, function (err, doc){
           if (err){
-            res.writeHead(302, {'Location': '/movies'})
+            res.writeHead(302, {'Location': '/notes/index'})
             res.end
             return
           }
-        res.writeHead(302, {'Location': '/movies'})
+        res.writeHead(302, {'Location': '/notes/index'})
         res.end()
       })
     })
   }
-  if (req.method === 'GET') {
-    movies.findOne({_id: url.params.id}, function (err, doc) {
+})
+routes.addRoute('/notes', (req, res, url) => { //posts new note to db
+if (req.method == 'POST') {
+  var data = ''
+  req.on('data', function (chunk) {
+    data += chunk
+  })
+  req.on('end', function () {
+    var note = qs.parse(data)
+    console.log(note)
+    notes.insert(note, function (err, doc) {
       if (err) {
-        res.end('shits broke yo')
+        console.log(err)
+        res.end('It may have broke vOv')
         return
       }
-      var template = view.render('show', doc )
-      res.end(template)
+      res.writeHead(302, {'Location': '/notes/index'})
+      res.end()
     })
-  }
+  })
+}
 })
 
 module.exports = routes
